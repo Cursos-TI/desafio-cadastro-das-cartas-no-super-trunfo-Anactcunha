0a6334fa83aedd10ab9ef5c3dfe04b6d8d4bbcf2
Ana Carolina Medeiros <anactcunha@gmail.com>
2025-09-21 00:09:23 -0300

    Tive a intensão de diminuir o código eliminando partes repetidas. Compilei as informações colocando em um tópico a apresentação, a comparação e o resultado de comparação das cartas que na versão anterior estavam divididas em blocos.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,23 +4,23 @@ int main(){
        
     
  // variáveis carta 1
-        int numerodacarta1, numerodospontosturisticos1;
-             float area1, pib1, densidade_populacional1, pib_per_capita1, super_Poder1;
+        int numerodacarta1 = 57, area1, numerodospontosturisticos1 = 25;
+             float pib1, densidade_populacional1, pib_per_capita1, super_Poder1;
                 unsigned long int populacao1; 
                     char estado1[50] = ("Paraná"), cidade1[50] = ("Londrina");  
                         area1 = 432;
-                            populacao1 = 1830795000;
+                            populacao1 = 18307950;
                                numerodospontosturisticos1 = 25;
                                   pib1 = 230060000.000;
                                         pib_per_capita1 = (pib1 / populacao1);
                                                 densidade_populacional1 = (populacao1 / area1);
 // variáveis carta 2                                             
-        int numerodacarta2, numerodospontosturisticos2;
-                float area2, pib2, densidade_populacional2, pib_per_capita2, super_Poder2;
+        int numerodacarta2 = 37, area2, numerodospontosturisticos2 = 103;
+              float pib2, densidade_populacional2,       pib_per_capita2, super_Poder2;
                     unsigned long int populacao2;
-                        char estado2[50], cidade2[50]; 
+                        char estado2[50] = ("Bahia"), cidade2[50] = ("Salvador"); 
                             area2 = 693;
-                                populacao2 = 2564204000;
+                                populacao2 = 256420400;
                                     numerodospontosturisticos2 = 103;
                                         pib2 = 629540000.000;
                                             pib_per_capita2 = (pib2 / populacao2);
@@ -32,7 +32,6 @@ int main(){
     // o usuário vai digitar o numero da carta que pegou
     scanf("%d", &numerodacarta1);
         
-        
     printf("Digite o nome do estado: \n");
     scanf ("%s", estado1);
     // o usuário digita o nome do estado que aparece na carta
@@ -42,56 +41,33 @@ int main(){
 
     printf("Digite a área da cidade em Km2: \n");
     // usuário digita numero da área da cidade 
-    scanf ("%f", &area1);
+    scanf ("%d", &area1);
    
-    
     printf("Digite a população da cidade: \n");
     // usuário digita a população da cidade
     scanf("%lu", &populacao1);
-    if (populacao1 > populacao2) {
-        printf("Carta 1 venceu!\n");
-    } else {
-         printf("Carta 2 venceu!\n");
-        }
+    
     printf("Digite o número de pontos turísticos: \n");
      // o usuário vai digitar o numero de pontos turísticos
     scanf("%d", &numerodospontosturisticos1);
-   if (numerodospontosturisticos1 > numerodospontosturisticos2) {
-        printf("Carta 1 venceu!\n");
-    } else {
-         printf("Carta 2 venceu!\n");
-        }
+  
     printf("Digite o pib da cidade: \n");
       // o usuário vai digitar o pib da cidade
     scanf("%f", &pib1);
-   if (pib1 > pib2) {
-        printf("Carta 1 venceu!\n");
-    } else {
-         printf("Carta 2 venceu!\n");
-        }
-
+   
     // cálculo pib per capita Carta 1
     pib_per_capita1 = (pib1 / populacao1);
     printf("O Pib per capita é: %.2f\n", pib_per_capita1);
-    if (pib_per_capita1 > pib_per_capita2) {
-        printf("Carta 1 venceu!\n");
-    } else {
-         printf("Carta 2 venceu!\n");
-        }
+    
     //cálculo densidade populacional Carta 1
     densidade_populacional1 = (populacao1 / area1);
     printf("A densidade populacional é: %f\n", densidade_populacional1);
-    if (densidade_populacional1 < densidade_populacional2) {
-        printf("Carta 1 venceu!\n");
-    } else {
-         printf("Carta 2 venceu!\n");
-        }
+    
 
+                printf("-------CARTA 1 CADASTRADA-------\n");
+   
 
-                                    printf("-------CARTA 1 CADASTRADA-------\n");
-    printf("----------------\n");
-
-                                        printf("Carta02 \n");
+    printf("Carta02 \n");
 
     printf("Digite o número da carta que pegou: \n");
     scanf(" %d", &numerodacarta2);
@@ -103,7 +79,7 @@ int main(){
     scanf ("%s", cidade2);
     // o usuário digita o nome da cidade que aparece na carta
     printf("Digite a área da cidade em Km2: \n");
-    scanf ("%f", &area2);
+    scanf ("%d", &area2);
     // usuário digita numero da área da cidade 
     printf("Digite a população da cidade: \n");
     scanf("%lu", &populacao2);
@@ -122,99 +98,65 @@ int main(){
     densidade_populacional2 = (populacao2 / area2);
     printf("A densidade populacional é: %f\n", densidade_populacional2);
     
+        printf("-------CARTA 2 CADASTRADA-------\n");
     
 
-    printf("-------CARTA 2 CADASTRADA-------\n");
-    printf("----------------\n");
+    printf(" -----------APRESENTAÇÃO/COMPARAÇÃO/RESULTADO COMPARAÇÃO DAS CARTAS-------------\n");
+/* a partir daqui no terminal serão apresentadas as informações de forma que elas estejam organizadas de maneira lógica e que o usuário consiga entender da melhor forma sem repetição de informações como estava na versão anterior*/
+    printf("Comparação de cartas (Atributo: Area em m2)\n");
 
-    printf("População: %lu\n", populacao1);
-    printf("Número dos pontos turísticos: %d\n", numerodospontosturisticos1);
-    printf("Pib: %.3f\n", pib1);
-    pib_per_capita1 = (pib1 / populacao1);
-    printf("O Pib per capita é: %.3f\n", pib_per_capita1);
-    densidade_populacional1 = (populacao1 / area1);
-    printf("A densidade populacional é: %.3f\n", densidade_populacional1);
+    printf("Carta 1 - Londrina: %d\n", area1);
+    printf("Carta 2 - Salvador: %d\n", area2);
+    if (area1 > area2){
+                printf("Resultado: Carta 1 (Londrina) venceu!\n");   
+                }else {  
+                printf("Resultado: Carta 2 (Salvador) venceu!\n");}
 
+    printf("Comparação de cartas (Atributo: População)\n");
+
+    printf("Carta 1 - Londrina: %lu\n", populacao1);
+    printf("Carta 2 - Salvador: %lu\n", populacao2);
+    if (populacao1 > populacao2){
+                printf("Resultado: Carta 1 (Londrina) venceu!\n");   
+                }else {  
+                printf("Resultado: Carta 2 (Salvador) venceu!\n");}
+
+    printf("Comparação de cartas (Atributo: Numero dos pontos turísticos)\n");
+    printf("Carta 1 - Londrina: %d\n", numerodospontosturisticos1);
+    printf("Carta 2 - Salvador: %d\n", numerodospontosturisticos2);
+    if (numerodospontosturisticos1 > numerodospontosturisticos2){
+                printf("Resultado: Carta 1 (Londrina) venceu!\n");   
+                }else {  
+                printf("Resultado: Carta 2 (Salvador) venceu!\n");}
+
+    printf("Comparação de cartas (Atributo: Pib)\n");
+
+    printf("Carta 1 - Londrina: %.3f\n", pib1);
+    printf("Carta 2 - Salvador: %.3f\n", pib2);
+    if (pib1 > pib2){
+                printf("Resultado: Carta 1 (Londrina) venceu!\n");   
+                }else {  
+                printf("Resultado: Carta 2 (Salvador) venceu!\n");}
+
+
+    printf("Comparação de cartas (Atributo: Pib per capita)\n");
+
+    printf("Carta 1 - Londrina: %.2f\n", pib_per_capita1);
+    printf("Carta 2 - Salvador: %.2f\n", pib_per_capita2);
+    if (pib_per_capita1 > pib_per_capita2){
+                printf("Resultado: Carta 1 (Londrina) venceu!\n");   
+                }else {  
+                printf("Resultado: Carta 2 (Salvador) venceu!\n");}
+
+    printf("Comparação de cartas (Atributo: Densidade populacional)\n");
+
+    printf("Carta 1 - Londrina: %.3f\n", densidade_populacional1);
+    printf("Carta 2 - Salvador: %.3f\n", densidade_populacional2);
+    if (densidade_populacional1 < densidade_populacional2){
+                printf("Resultado: Carta 1 (Londrina) venceu!\n");   
+                }else {  
+                printf("Resultado: Carta 2 (Salvador) venceu!\n");}
        
-
-
-    printf("-------CARTA 1 APRESENTADA-------\n");
-
-    printf("População: %lu\n", populacao2);
-    printf("Número dos pontos turísticos: %d\n", numerodospontosturisticos2);
-    printf("Pib: %.3f\n", pib2);
-    pib_per_capita2 = (pib2 / populacao2);
-    printf("O Pib per capita é: %.3f\n", pib_per_capita2);
-    densidade_populacional2 = (populacao2 / area2);
-    printf("A densidade populacional é: %.3f\n", densidade_populacional2);
-
-
-    printf("-------CARTA 2 APRESENTADA-------\n");
-    printf("----------------\n");
-
-                                printf("COMPARAÇÃO DAS CARTAS2\n");
-                //comparação utilizando if / else;
-
-                 
-    printf("Area de Londrina > Area de Salvador: %d\n", area1 > area2);
-                 if (area1 > area2) {
-        printf("Carta 1 venceu!\n");
-    } else {
-         printf("Carta 2 venceu!\n");
-        }
-                printf("populacao de Londrina > populacao de Salvador: %d\n", populacao1 > populacao2);
-                printf("Pontos turísticos de Londrina > pontosturisticos de Salvador: %d\n", numerodospontosturisticos1 > numerodospontosturisticos2);
-                printf("Pib Londrina > Pib Salvador: %d\n", pib1 > pib2);
-                printf("pib_per_capita  Londrina > pib_per_capita Salvador: %d\n", pib_per_capita1 >= pib_per_capita2);
-                printf("densidade_populacional  Londrina < densidade_populacional Salvador: %d\n", densidade_populacional1 <= densidade_populacional2);
-
-                printf("COMPARAÇÃO DAS CARTAS CARTA 2 / CARTA 1\n");
-
-                printf("area2 > area1: %d\n", area2 > area1);
-                printf("populacao2 > populacao1: %d\n", populacao2 > populacao1);
-                printf("numerodospontosturisticos2 > numerodospontosturisticos1: %d\n", numerodospontosturisticos2 > numerodospontosturisticos1);
-                printf("pib2 > pib1: %d\n", pib2 > pib1);
-                printf("pib_per_capita2 > pib_per_capita1: %d\n", pib_per_capita2 >= pib_per_capita1);
-                printf("densidade_populacional2 < densidade_populacional1: %d\n", densidade_populacional2 <= densidade_populacional1);
-
-                printf("---------------RESULTADO DA COMPARAÇÃO DAS CARTAS\n-------------");
-                // aqui a comparação das cartas irá gerar a mensagem que informa qual carta venceu
-                printf("Área:\n");
-                if (area1 > area2){
-                printf("Carta 1 venceu!\n");   
-                }else {  
-                printf("Carta 2 venceu!\n");}
-                
-                printf("populacao:\n");
-                if (populacao1 > populacao2){
-                printf("Carta 1 venceu!\n");   
-                }else {  
-                printf("Carta 2 venceu!\n");}
-                
-                printf("Número de pontos turísticos: \n");
-                if (numerodospontosturisticos1 > numerodospontosturisticos2) {
-                printf("Carta 1 venceu!\n");   
-                }else {
-                printf("Carta 2 venceu!\n");} 
-                
-                printf("Pib da cidade: \n");
-                if (pib1 > pib2) {
-                printf("Carta 1 venceu!\n");  
-                } else { 
-                printf("Carta 2 venceu!\n");}
-
-                printf("Pib per capita: \n");
-                if (pib_per_capita1 > pib_per_capita2) {
-                printf("Carta 1 venceu!\n");   
-                } else {  
-                printf("Carta 2 venceu!\n");}
-                
-                printf("Densidade populacional: ");
-                if (densidade_populacional1 < densidade_populacional2) {
-                printf("Carta 1 venceu!\n");   
-                } else {  
-                printf("Carta 2 venceu!\n");
-                }
 
                 return 0;
                 }
