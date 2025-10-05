0cc52f52972a70332e6cbed641398bc078dcc213
Ana Carolina Medeiros <anactcunha@gmail.com>
2025-10-04 21:11:49 -0300

    retirei itens do menu que achei desnecessário por apresentar durente o jogo informaçõess repetidas e criei o switch com a opçõ selecionada e os dados de cada cidade para posterior comparação.

---
+++ READMElogica.md
@@ -0,0 +1,12 @@
+# Desafio Super Trunfo - Lógica do Jogo - Instruções
+
+- Menu Interativo: o menu interativo no terminal com a estrutura switch permite que o jogador
+escolha qual atributo será usado para comparar as cartas já cadastradas anteriormente. 
+
+- Comparação de Atributos: a comparação entre duas cartas será feita de acorodo com o atributo selecionado pelo jogador.
+
+- Regras de Comparação: vence a carta com o maior valor no atributo escolhido. Já na Densidade Demográfica, a regra inverte: vence a carta com o menor valor.
+
+- Exibição do Resultado: aparecerá na tela, o resultado da comparação, ex: O nome dos dois países, o atributo usado na comparação, os valores do atributo para cada carta, qual a carta vencedora. Caso aconteça empate será exibido a mensagem "Empate!"
+
+
--- STLogicadoJogo_Aventureiro.c
+++ STLogicadoJogo_Aventureiro.c
@@ -24,21 +24,75 @@ int main(){
                         printf("MENU INTERATIVO\n");
 // o menu interativo tem o objetivo de apresentar os atributos a carta de cada jogador tem para que seja feita a comparação
                         printf("Menu de opções:\n");
-                        printf("1. Cadastrar nova carta?\n");
-                        printf("2. Ver regras do jogo:\n"); 
-                        printf("3. Apresentar o número da carta:\n");
-                        printf("4. Apresentar o nome do Estado e da Cidade:\n");
-                        printf("5. Apresentar a população:\n");
-                        printf("6. Apresentar a área:\n");
-                        printf("7. Apresentar o Pib:\n");
-                        printf("8. Apresentar o número de pontos turisticos:\n");
-                        printf("9. Apresentar o Pib per capita:\n");
-                        printf("10. Apresentar a densidade populacional:\n");
-                        printf("Escolha uma opção;\n");
-                        scanf("%d",&opcao);
- 
+                        printf("1. Ver regras do jogo\n"); 
+                        printf("2. Apresentar a população\n");
+                        printf("3. Apresentar a área\n");
+                        printf("4. Apresentar o Pib\n");
+                        printf("5. Apresentar o número de pontos turisticos\n");
+                        printf("6. Apresentar o Pib per capita\n");
+                        printf("7. Apresentar a densidade populacional\n");
+                        printf("8. Sair do jogo.\n");
+                        printf("Escolha uma opção:\n");
+                        scanf("%d", &opcao);
+        switch (opcao)
+        {
+        case 1:
+        
+            printf("Ver as regras do jogo\n");
+            printf("Sempre que uma opção for escolhida, os atributos serão comparados entre as cartas dos dois jogadores, o jogador com o maior valor de atributo escolhido vence a rodada.\n");
+            break;
+         
+        case 2:
+        
+            printf("Apresentar a população\n");
+            printf("%lu - %s\n", populacao1, cidade1); 
+            printf("%lu - %s\n", populacao2, cidade2);
+            break; 
+        
+        case 3:
+            printf("Apresentar a área\n");
+            printf("%d - %s\n", area1, cidade1); 
+            printf("%d - %s\n", area2, cidade2);
+            break; 
+        
+        case 4:
+            printf("Apresentar o Pib\n");
+            printf("%f - %s\n", pib1, cidade1); 
+            printf("%f - %s\n", pib2, cidade2);
+            break; 
+            
+        case 5:
+            printf("Apresentar o número de pontos turísticos\n");
+            printf("%d - %s\n", numerodospontosturisticos1, cidade1); 
+            printf("%d - %s\n", numerodospontosturisticos2, cidade2);
+            break; 
+        
+        case 6:
+            printf("Apresentar o Pib per capita\n");
+            pib_per_capita1 = (pib1 / populacao1);
+            printf("O Pib per capita é: %.2f - %s\n", pib_per_capita1, cidade1);
+            pib_per_capita2 = (pib2 / populacao2);
+            printf("O Pib per capita é: %.2f - %s\n", pib_per_capita2, cidade2);
+            break; 
+        
+        case 7:
+            printf("Apresentar a densidade populacional\n");
+            densidade_populacional1 = (populacao1 / area1);
+            printf("A densidade populacional é: %f - %s\n", densidade_populacional1, cidade1);
+            densidade_populacional2 = (populacao2 / area2);
+            printf("A densidade populacional é: %f - %s\n", densidade_populacional2, cidade2);
+            break; 
 
-       
+        case 8:
+            printf("Saindo do Jogo...\n");
+               
 
-                return 0;
+           
                 }
+               
+                        
+       
+            
+        
+         return 0;
+        }
