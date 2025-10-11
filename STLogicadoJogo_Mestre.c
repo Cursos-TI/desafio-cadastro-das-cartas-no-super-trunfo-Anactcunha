e0fc8f0e353ab5338f0f47b991f02b1e08b7862a
Ana Carolina Medeiros <anactcunha@gmail.com>
2025-10-11 17:41:11 -0300

    corrigindo erros que não deixam o programa rodar. Erros na declaração das variávies e na estrutura dos if -else.

--- STLogicadoJogo_Mestre.c
+++ STLogicadoJogo_Mestre.c
@@ -3,8 +3,9 @@
 #include <time.h>
 
 int main() {
-    int escolha1, escolha2;
-    int indiceCartaComputador;
+    int numeroJogador, numeroComputador, escolha1, escolha2, tipoComparacao;
+    int resultado;
+    int cartaComputador;
 
 // Dados das cartas (usando variaveis individuais para a comparação)
     int numerodacarta1 = 57;
@@ -13,39 +14,58 @@ int main() {
     unsigned long populacao1 = 18307950;
     unsigned long populacao2 = 256420400;
 
-    int pontosTuristicos1 = 25;
-    int pontosTuristicos2 = 103;
+    int numerodepontosturisticos1 = 25;
+    int numerodepontosturisticos2 = 103;
 
     // variáveis carta 1 e 2 (as mesmas variáveis foram usadas no nível iniciante e intermediário.) 
 
 //gerar número aleatório pois a comparação será entre carta escolhida pelo jogador e carta do computador
     srand(time(0));
-    numeroComputador = rand() %5 +1; //número entre 1 e 5
-
-    
-
+    cartaComputador = rand() %2; //para que o computador escolha entre a carta 1 e a carta2
 
 // realização do desafio em desenvolvendo a lógica do jogo nível mestre
                         printf("MENU INTERATIVO\n");
 /* Criação do menu interativo com a escolha de duas opções, o uso so switch foi a proposta da atividade com o objetivo de apresentar os atributos de carta que cada jogador tem para que seja feita a comparação*/
                         
 //inicio do jogo
-                        printf("Bem-vindo ao jogo Super Trunfo nível Mestre!\n");
+        printf("Bem-vindo ao jogo Super Trunfo nível Mestre!\n");
                         // itens para escolha dos jogadores
-                        printf("Você deve escolher um número e o tipo de comparação.\n");
-                        printf("1. Número da carta:\n");
-                        printf("2. População\n");
-                        printf("3. Número de pontos turisticos\n");
-                        printf("4. Sair do Jogo\n");
-                        printf("Regras do jogo\n");
-                        printf("Sempre que uma opção for escolhida, os atributos serão comparados entre as cartas dos dois jogadores, o jogador com o maior valor de atributo escolhido vence a rodada, exeto na densidade populacional, que vencerá quem tiver o menor valor.\n");
+        printf("Sempre que uma opção for escolhida, os atributos serão comparados entre as cartas dos dois jogadores, o jogador com o maior valor de atributo escolhido vence a rodada, exeto na densidade populacional, que vencerá quem tiver o menor valor.\n");
+        
+        printf("Escolha um atributo para comparação.\n");
+                        
             //neste caso temos a apresentação com comparação de atributos.
-                        printf("ESCOLHA A PRIMEIRA OPÇÃO:\n"); // aqui será inserido o número da opção escolhida
-                        scanf("%d", &numeroJogador);
-                        scanf("%d", &numeroComputador); // irá apresentar o número da opção escolhida
-                        printf("ESCOLHA A SEGUNDA OPÇÃO:\n"); // idem primeira opção
-                        scanf("%d", &numeroJogador);
-                        scanf("%d", &numeroComputador); // idem primeira opção
+         
+        printf("1. Numero da carta:\n");
+        printf("2. Populacao:\n");
+        printf("3. Numero de pontos turisticos:\n");
+        printf("Escolha a primeira opção:\n");
+        scanf("%d", &escolha1);  // irá apresentar o número da opção escolhida
+        printf("Escolha a segunda opção:\n"); // idem primeira opção
+        scanf("%d", &escolha2); // idem primeira opção
+            if (escolha1 == 1) {
+                printf("1. numero da carta:\n");
+                printf("%d\n", numerodacarta1); 
+                printf("numero da carta do computador:\n", "%d\n", numerodacarta2);
+                printf("3. numero de pontos turisticos\n");
+                printf("%d\n", numerodepontosturisticos1);
+                printf("numero da carta do computador:\n","%d\n", numerodepontosturisticos2);
+
+            } else if (escolha1 == 2) {
+                printf("2. População\n");
+                printf("%lu\n", populacao1);
+                printf("numero da carta do computador:\n", "%lu\n", populacao2);
+                printf("3. Numero de pontos turísticos\n");
+                printf("%d\n", numerodepontosturisticos1);
+                printf("numero da carta do computador:\n", "%d\n", numerodepontosturisticos2);
+            } else if (escolha1 == 3) {
+                printf("1. numero da carta\n");
+                printf("%d\n", numerodacarta1);
+                printf("numero da carta do computador:\n", "%d\n", numerodacarta2);
+                printf("2. população\n");
+                printf("%lu\n", populacao1);
+                printf("numero da carta do computador:\n", "%lu\n", populacao2);
+            }
                         
                         
             switch (tipoComparacao)
@@ -53,30 +73,43 @@ int main() {
                   
             case 1:
             /* no case 2 teremos a apresentação da opção do jogador com a apresentação subsequente dos valores do atributo escolhido e o if - if else e else trarão a comparação e a condição, com a opção de qualquer uma das duas vencer e a opção do empate caso nenhuma das condicionantes de  > 1 ou > 2 aconteçam*/
+            if (numerodacarta1 > numerodacarta2) {
+                printf("Carta 1 venceu!\n");
+            } else if (numerodacarta1 < numerodacarta2) {
+                printf("Carta 2 venceu!\n");
+            } else if (numerodacarta1 == numerodacarta2) {
+                printf("As cartas empataram!\n");
+            }
+
             printf("Você escolheu a opção maior!\n");
             resultado = numerodacarta1 > numerodacarta2 ? 1 : 0;
             break;
           
             case 2:
+            if (populacao1 > populacao2) {
+                printf("Carta 1 venceu!\n");
+            } else if (populacao1 < populacao2) {
+                printf("Carta 2 venceu!\n");
+            } else if (populacao1 == populacao2) {
+                printf("As cartas empataram!\n");
+            }
             printf("Você escolheu a opção maior!\n");
             resultado = populacao1 > populacao2 ? 1 : 0;
             break;
             
             case 3:
+            if (numerodepontosturisticos1 > numerodepontosturisticos2) {
+                printf("Carta 1 venceu!\n");
+            } else if (numerodepontosturisticos1 < numerodepontosturisticos2) {
+                printf("Carta 2 venceu!\n");
+            } else  if (numerodepontosturisticos1 == numerodepontosturisticos2) {
+                printf("As cartas empataram!\n");
+            }
         
             printf("Você escolheu a opção maior!\n");
-            resultado = numerodospontoturisticos1 > numerodospontosturisticos2 ? 1 : 0;
+            resultado = numerodepontosturisticos1 > numerodepontosturisticos2 ? 1 : 0;
             break;
             
-            case 4:
-            printf("Você escolheu a opção maior!\n");
-            resultado = numeroJogador > numeroComputador ? 1 : 0;
-            break;
-
-            case 5: //opção de sair do jogo.
-            printf("Saindo do Jogo...\n");
-            break;
-
             default: // caso o jogoador insira uma opção que não tenha sido listada no menu.
             printf("Opção inválida. Tente novamente.\n");
             break;
--- rascunho.c
+++ rascunho.c
@@ -1,1 +1,89 @@
-float pib1 = 230060000.000, densidade_populacional1, pib_per_capita1;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int escolha1, escolha2;
+    int cartaComputador;
+    long somaJogador = 0;
+    long somaComputador = 0;
+
+    // Dados das cartas
+    int numerodacarta1 = 57;
+    int numerodacarta2 = 37;
+    unsigned long populacao1 = 18307950;
+    unsigned long populacao2 = 256420400;
+    int numerodepontosturisticos1 = 25;
+    int numerodepontosturisticos2 = 103;
+
+    srand(time(0));
+    cartaComputador = rand() % 2; // 0 ou 1
+
+    printf("Bem-vindo ao jogo Super Trunfo nivel Mestre!\n");
+    printf("Escolha DOIS atributos para comparacao.\n");
+
+    // Lógica para mostrar qual carta é de quem
+    if (cartaComputador == 0) {
+        printf("Sua carta: %d | Carta do computador: %d\n", numerodacarta2, numerodacarta1);
+    } else {
+        printf("Sua carta: %d | Carta do computador: %d\n", numerodacarta1, numerodacarta2);
+    }
+
+    printf("Primeira escolha de atributo:\n");
+    printf("1. Numero da carta\n");
+    printf("2. Populacao\n");
+    printf("3. Numero de pontos turisticos\n");
+    scanf("%d", &escolha1);
+
+    printf("Segunda escolha de atributo:\n");
+    if (escolha1 == 1) {
+        printf("2. Populacao\n");
+        printf("3. Numero de pontos turisticos\n");
+    } else if (escolha1 == 2) {
+        printf("1. Numero da carta\n");
+        printf("3. Numero de pontos turisticos\n");
+    } else if (escolha1 == 3) {
+        printf("1. Numero da carta\n");
+        printf("2. Populacao\n");
+    }
+    scanf("%d", &escolha2);
+
+    // Soma dos atributos (primeira escolha)
+    if (escolha1 == 1) {
+        somaJogador += (cartaComputador == 0) ? numerodacarta2 : numerodacarta1;
+        somaComputador += (cartaComputador == 0) ? numerodacarta1 : numerodacarta2;
+    } else if (escolha1 == 2) {
+        somaJogador += (cartaComputador == 0) ? populacao2 : populacao1;
+        somaComputador += (cartaComputador == 0) ? populacao1 : populacao2;
+    } else if (escolha1 == 3) {
+        somaJogador += (cartaComputador == 0) ? numerodepontosturisticos2 : numerodepontosturisticos1;
+        somaComputador += (cartaComputador == 0) ? numerodepontosturisticos1 : numerodepontosturisticos2;
+    }
+
+    // Soma dos atributos (segunda escolha)
+    if (escolha2 == 1) {
+        somaJogador += (cartaComputador == 0) ? numerodacarta2 : numerodacarta1;
+        somaComputador += (cartaComputador == 0) ? numerodacarta1 : numerodacarta2;
+    } else if (escolha2 == 2) {
+        somaJogador += (cartaComputador == 0) ? populacao2 : populacao1;
+        somaComputador += (cartaComputador == 0) ? populacao1 : populacao2;
+    } else if (escolha2 == 3) {
+        somaJogador += (cartaComputador == 0) ? numerodepontosturisticos2 : numerodepontosturisticos1;
+        somaComputador += (cartaComputador == 0) ? numerodepontosturisticos1 : numerodepontosturisticos2;
+    }
+
+    // Resultado final
+    printf("\n--- Resultados ---\n");
+    printf("Soma da sua carta: %ld\n", somaJogador);
+    printf("Soma da carta do computador: %ld\n", somaComputador);
+
+    if (somaJogador > somaComputador) {
+        printf("Parabéns, você venceu!\n");
+    } else if (somaComputador > somaJogador) {
+        printf("Infelizmente, você perdeu!\n");
+    } else {
+        printf("Empate!\n");
+    }
+
+    return 0;
+}
